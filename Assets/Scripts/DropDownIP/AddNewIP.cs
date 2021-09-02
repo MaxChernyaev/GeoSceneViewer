ae85e7aa152af1b2ffef252bcf1fa18f6bdafd02
MaxChernyaev <dilbert.main@mail.ru>
2021-09-02 12:22:50 +0300

    Забытые файлы для последнего коммита (сохранение IP в json)

---
+++ Assets/Scripts/DropDownIP/AddNewIP.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddNewIP : MonoBehaviour
+{
+    private IPListSaver ip_saver = new IPListSaver();
+    [SerializeField] private InputField inputNameIP;
+    [SerializeField] private InputField inputIPAddress;
+    [SerializeField] private Text ErrorNotification;
+
+    public void AddNewIPFromJson()
+    {
+        if (inputIPAddress.text != "" && inputNameIP.text == "") // не ввели название сети, но ввели сам IP
+        {
+            ErrorNotification.text = "";
+            inputNameIP.text = inputIPAddress.text;
+            AddIP();
+        }
+        if (inputIPAddress.text == "" && inputNameIP.text != "") // не ввели IP, но ввели название
+        {
+            ErrorNotification.text = "Вы не ввели IP адрес =(";
+        }
+        if (inputIPAddress.text == "" && inputNameIP.text == "") // не ввели ничего
+        {
+            ErrorNotification.text = "Вы ничего не ввели =(";
+        }
+        if (inputIPAddress.text != "" && inputNameIP.text != "") // ввели оба поля
+        {
+            ErrorNotification.text = "";
+            AddIP();
+        }
+    }
+
+    private void AddIP()
+    {
+        ip_saver.AddIPList(inputNameIP.text, inputIPAddress.text);
+        ip_saver.IPListSaveToJson();
+        GameObject.Find("Dropdown").GetComponent<DropDownMenu>().UpdateDropDown();
+    }
+
+}
